351a47f0b5d95b37f3eb8dd39a82499f5d05300b
mouayad03 <116551140+mouayad03@users.noreply.github.com>
2023-04-03 14:09:57 +0200

    kubernetes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+import express from 'express';
+import http from 'http';
+import { Request, Response } from 'express';
+import os from 'os';
+
+// Create the express server
+const app = express();
+const server = http.createServer(app);
+
+// route for / that returns a simple html page with the hostname
+app.get('/', (req: Request, res: Response) => {
+  res.send(`<h1>Hello World!</h1><p>Host: ${os.hostname()}</p>`);
+});
+
+const serverPort = process.env.PORT || 3000;
+server.listen(serverPort, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Express Server started on port ${serverPort}`);
+});
